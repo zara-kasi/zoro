61f9704379490e3784df6f81ac5d307d3a31fdcc
zarakasi <198352777+zara-kasi@users.noreply.github.com>
2025-07-15 16:24:36 +0530

    update main.js

--- main.js
+++ main.js
@@ -10,45 +10,35 @@ import {
 
 // Settings
 import { DEFAULT_SETTINGS } from './settings/defaultSettings.js';
-
 import { ZoroSettingTab } from './settings/settingsTab.js';
+import { validateSettings, saveSettings, loadSettings } from './settings/helpers.js';
+import { createSampleNotes } from './utils/sampleNotes.js';
 
 // Utils 
 import { RequestQueue } from './utils/requestQueue.js';
-
 import { pruneCache, getFromCache, setToCache, clearCacheForMedia } from './utils/cache.js';
 
 // API ListManager 
 import { authenticateUser, exchangeCodeForToken, makeObsidianRequest, testAccessToken, getAuthenticatedUsername, handleAuthMessage } from './api/auth.js';
-
 import { getMediaListQuery, getSingleMediaQuery, getUserStatsQuery, getSearchMediaQuery, getZoroUrl } from './api/graphql.js';
-
 import { fetchZoroData } from './api/fetchZoroData.js';
-
 import { updateMediaListEntry, checkIfMediaInList, addMediaToList } from './api/listManager.js';
-
 
 // UI: Renderers 
 import { renderMediaList, createMediaCard, createDetailsRow, renderTableLayout } from './ui/render/renderList.js';
-
 import { renderSingleMedia } from './ui/render/renderSingle.js';
-
 import { renderUserStats } from './ui/render/renderStats.js';
-
 
 // UI: Modals 
 import { createAddModal, createEditModal, createAuthenticationPrompt, ClientIdModal,
   ClientSecretModal,
   AuthPinModal } from './ui/modals.js';
   
-  
   // UI: Search Interface 
   import { renderSearchInterface, renderSearchResults, handleAddClick } from './ui/searchInterface.js';
   
-  
   // UI: helpers
   import { handleEditClick, injectCSS, renderError, fetchData, renderZoroData, processZoroSearchCodeBlock, processInlineLinks  } from './ui/helpers.js';
-  
   
 // Parsers 
 import { parseCodeBlockConfig, parseSearchCodeBlockConfig } from './parsers/parseCodeBlock.js';
@@ -57,18 +47,9 @@ import { parseInlineLink } from './parsers/parseInlineLink.js';
 // Controllers 
 import { processZoroCodeBlock } from './controllers/codeBlockProcessor.js';
 
-
-// Additional Featues
-import { validateSettings, saveSettings, loadSettings } from './settings/helpers.js';
-
-import { createSampleNotes } from './utils/sampleNotes.js';
-
-
-
 // Plugin Class 
 class ZoroPlugin extends Plugin { 
-
-  // Constructor 
+  
   constructor(app, manifest) {
     super(app, manifest);
     
@@ -81,11 +62,10 @@ class ZoroPlugin extends Plugin {
     this.requestQueue = RequestQueue();
     this.cacheTimeout = 5 * 60 * 1000;
 
-  // Add periodic pruning
+  // periodic pruning
   this.pruneInterval = setInterval(() => pruneCache(this.cache), this.cacheTimeout);
   }
-  
- // On Load
+
   async onload() {
     console.log('[Zoro] Plugin loading...');
 
@@ -109,7 +89,7 @@ class ZoroPlugin extends Plugin {
     this.registerMarkdownCodeBlockProcessor('zoro', this.processZoroCodeBlock.bind(this));
     this.registerMarkdownCodeBlockProcessor('zoro-search', this.processZoroSearchCodeBlock.bind(this));
 
-    // Process inline links (e.g., [[Zoro:ID]])
+    // Process inline links
     this.registerMarkdownPostProcessor(this.processInlineLinks.bind(this));
 
     // Add plugin settings tab
@@ -118,7 +98,6 @@ class ZoroPlugin extends Plugin {
     console.log('[Zoro] Plugin loaded successfully.');
   }
 
-  //  unload 
   onunload() {
     console.log('Unloading Zoro Plugin');
 
@@ -139,7 +118,6 @@ class ZoroPlugin extends Plugin {
       
     }
   }
-
 } 
 
 export default ZoroPlugin;

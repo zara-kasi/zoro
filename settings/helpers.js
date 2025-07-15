1c7e641d2330148e51700b25a29450f0f2f206ab
zarakasi <198352777+zara-kasi@users.noreply.github.com>
2025-07-15 13:44:00 +0530

    update settings/helpers.js

---
+++ settings/helpers.js
@@ -0,0 +1,16 @@
+export function validateSettings(settings) {
+    return {
+      defaultUsername: typeof settings?.defaultUsername === 'string' ? settings.defaultUsername : '',
+      defaultLayout: ['card', 'list'].includes(settings?.defaultLayout) ? settings.defaultLayout : 'card',
+      showCoverImages: !!settings?.showCoverImages,
+      showRatings: !!settings?.showRatings,
+      showProgress: !!settings?.showProgress,
+      showGenres: !!settings?.showGenres,
+      gridColumns: Number.isInteger(settings?.gridColumns) ? settings.gridColumns : 3,
+      clientId: typeof settings?.clientId === 'string' ? settings.clientId : '',
+      clientSecret: typeof settings?.clientSecret === 'string' ? settings.clientSecret : '',
+      redirectUri: typeof settings?.redirectUri === 'string' ? settings.redirectUri : DEFAULT_SETTINGS.redirectUri,
+      accessToken: typeof settings?.accessToken === 'string' ? settings.accessToken : '',
+    };
+  }
+  

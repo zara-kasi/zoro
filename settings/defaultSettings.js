731914cda2be58bc9e90c6f74b85e0d18a701ec8
zarakasi <198352777+zara-kasi@users.noreply.github.com>
2025-07-15 08:19:48 +0530

    update settings/defaultSettings.js

---
+++ settings/defaultSettings.js
@@ -0,0 +1,13 @@
+export const DEFAULT_SETTINGS = {
+  defaultUsername: '',
+  defaultLayout: 'card',
+  showCoverImages: true,
+  showRatings: true,
+  showProgress: true,
+  showGenres: false,
+  gridColumns: 2,
+  clientId: '',
+  clientSecret: '',
+  redirectUri: 'https://anilist.co/api/v2/oauth/pin',
+  accessToken: '',
+};

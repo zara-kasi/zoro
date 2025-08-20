fe7dc871e42dcefaa2de9f6d4a320d31f61d585a
zarakasi <198352777+zara-kasi@users.noreply.github.com>
2025-08-20 12:24:17 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import ZoroPlugin from './src/core/ZoroPlugin.js';
+
+// Export for Obsidian (CommonJS format)
+module.exports = {
+  default: ZoroPlugin,
+};
+
+// Also export for ES6 modules (future-proofing)
+export default ZoroPlugin;

1d5716c1701d3694a7788ac45f62bc8c2249a72c
zarakasi <198352777+zara-kasi@users.noreply.github.com>
2025-08-20 15:06:37 +0530

    Update SearchRenderer.js

--- src/rendering/renderers/SearchRenderer.js
+++ src/rendering/renderers/SearchRenderer.js
@@ -1,3 +1,5 @@
+const { Plugin, PluginSettingTab, Setting, Notice, requestUrl, Modal, setIcon } = require('obsidian');
+
 class SearchRenderer {
   constructor(parentRenderer) {
     this.parent = parentRenderer;

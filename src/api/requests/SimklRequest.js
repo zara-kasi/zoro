25990a464622931177e72c459e55194237b07a82
zarakasi <198352777+zara-kasi@users.noreply.github.com>
2025-08-20 14:52:57 +0530

    Update SimklRequest.js

--- src/api/requests/SimklRequest.js
+++ src/api/requests/SimklRequest.js
@@ -1,3 +1,5 @@
+const { Plugin, PluginSettingTab, Setting, Notice, requestUrl, Modal, setIcon } = require('obsidian');
+
 class SimklRequest {
   constructor(config, plugin) {
     this.config = config;

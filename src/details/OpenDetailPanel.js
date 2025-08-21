7ddcd7d89a946664a0f057245b0fcf6182fcf959
zarakasi <198352777+zara-kasi@users.noreply.github.com>
2025-08-21 14:54:24 +0530

    update src/details/OpenDetailPanel.js

--- src/details/OpenDetailPanel.js
+++ src/details/OpenDetailPanel.js
@@ -1,48 +1,49 @@
-const { Plugin, PluginSettingTab, Setting, Notice, requestUrl, Modal, setIcon } = require('obsidian');
+// No obsidian imports needed
+import { RenderDetailPanel } from './RenderDetailPanel.js';
+import { DetailPanelSource } from './DetailPanelSource.js';
 
 class OpenDetailPanel {
-  constructor(plugin) {
-    this.plugin = plugin;
-    this.currentPanel = null;
-    this.boundOutsideClickHandler = this.handleOutsideClick.bind(this);
-    this.renderer = new RenderDetailPanel(plugin);
-    this.dataSource = new DetailPanelSource(plugin);
-  }
+	constructor(plugin) {
+		this.plugin = plugin;
+		this.currentPanel = null;
+		this.boundOutsideClickHandler = this.handleOutsideClick.bind(this);
+		this.renderer = new RenderDetailPanel(plugin);
+		this.dataSource = new DetailPanelSource(plugin);
+	}
 
-  async showPanel(media, entry = null, triggerElement) {
-    this.closePanel();
-    const panel = this.renderer.createPanel(media, entry);
-    this.currentPanel = panel;
-    this.renderer.positionPanel(panel, triggerElement);
-    const closeBtn = panel.querySelector('.panel-close-btn');
-    if (closeBtn) closeBtn.onclick = () => this.closePanel();
-    document.body.appendChild(panel);
-    document.addEventListener('click', this.boundOutsideClickHandler);
-    this.plugin.requestQueue.showGlobalLoader();
+	async showPanel(media, entry = null, triggerElement) {
+		this.closePanel();
+		const panel = this.renderer.createPanel(media, entry);
+		this.currentPanel = panel;
+		this.renderer.positionPanel(panel, triggerElement);
+		const closeBtn = panel.querySelector('.panel-close-btn');
+		if (closeBtn) closeBtn.onclick = () => this.closePanel();
+		document.body.appendChild(panel);
+		document.addEventListener('click', this.boundOutsideClickHandler);
+		this.plugin.requestQueue.showGlobalLoader();
 
-    if (this.dataSource.shouldFetchDetailedData(media)) {
-      this.dataSource.fetchAndUpdateData(media.id, entry, (detailedMedia, malData, imdbData) => {
-        if (this.currentPanel === panel) this.renderer.updatePanelContent(panel, detailedMedia, malData, imdbData);
-      }).finally(() => this.plugin.requestQueue.hideGlobalLoader());
-    } else {
-      this.plugin.requestQueue.hideGlobalLoader();
-    }
-    return panel;
-  }
+		if (this.dataSource.shouldFetchDetailedData(media)) {
+			this.dataSource.fetchAndUpdateData(media.id, entry, (detailedMedia, malData, imdbData) => {
+				if (this.currentPanel === panel) this.renderer.updatePanelContent(panel, detailedMedia, malData, imdbData);
+			}).finally(() => this.plugin.requestQueue.hideGlobalLoader());
+		} else {
+			this.plugin.requestQueue.hideGlobalLoader();
+		}
+		return panel;
+	}
 
-  handleOutsideClick(event) {
-    if (this.currentPanel && !this.currentPanel.contains(event.target)) this.closePanel();
-  }
+	handleOutsideClick(event) {
+		if (this.currentPanel && !this.currentPanel.contains(event.target)) this.closePanel();
+	}
 
-  closePanel() {
-    if (this.currentPanel) {
-      this.renderer.cleanupCountdowns(this.currentPanel);
-      document.removeEventListener('click', this.boundOutsideClickHandler);
-      this.currentPanel.remove();
-      this.currentPanel = null;
-    }
-  }
+	closePanel() {
+		if (this.currentPanel) {
+			this.renderer.cleanupCountdowns(this.currentPanel);
+			document.removeEventListener('click', this.boundOutsideClickHandler);
+			this.currentPanel.remove();
+			this.currentPanel = null;
+		}
+	}
 }
 
-
-module.exports = { OpenDetailPanel };
+export { OpenDetailPanel };

6166501b284b7e008f809333026e0b91baca07ca
zarakasi <198352777+zara-kasi@users.noreply.github.com>
2025-08-24 19:28:25 +0530

    update src/features/Sample.js: change the first file for anime and Manga to trending

--- src/features/Sample.js
+++ src/features/Sample.js
@@ -15,12 +15,12 @@ class Sample {
                 name: 'Anime',
                 files: ['Watching.md', 'Planning.md', 'Re-watching .md', 'On Hold.md', 'Completed.md', 'Dropped.md',
                 'Trending.md','Stats.md'],
-                firstFile: 'Watching.md'
+                firstFile: 'Trending.md'
             },
             {
                 name: 'Manga', 
                 files: ['Reading.md', 'Planning.md', 'Re-reading.md', 'On Hold.md', 'Completed.md', 'Dropped.md','Trending.md', 'Stats.md'],
-                firstFile: 'Reading.md'
+                firstFile: 'Trending.md'
             },
              {
                 name: 'Movie',

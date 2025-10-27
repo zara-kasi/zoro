2a9ed9562ba7e605279e1d8754ddc9a739eb6f49
zarakasi <198352777+zara-kasi@users.noreply.github.com>
2025-10-27 17:12:54 +0530

    Change entry point from main.ts to index.js

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -15,7 +15,7 @@ const context = await esbuild.context({
 	banner: {
 		js: banner,
 	},
-	entryPoints: ["src/main.ts"],
+	entryPoints: ["src/index.js"],
 	bundle: true,
 	external: [
 		"obsidian",
